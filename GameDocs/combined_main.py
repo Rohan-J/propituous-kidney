acf954b3e5eac17aa8c092350ea74686d202c83a
thing1st <hanmathew@yahoo.com>
2015-11-07 15:46:10 -0800

    rasdasd

--- GameDocs/combined_main.py
+++ GameDocs/combined_main.py
@@ -1,19 +1,31 @@
 import pygame, sys
 
-pygame.init()
 
-DISPLAY = (1366,768)
+
+#-Constants
+WIDTH = 1366
+HEIGHT = 768
+DISPLAY = (WIDTH,HEIGHT)
 DEPTH = 32
 FLAGS = 0
 
-screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
 
-while True:
+def main():
+	pygame.init()
 
-	for event in pygame.event.get():
-		if event.type ==pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+	#-Variables
+	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
+	pygame.display.set_caption("rohan is hot")
+	screen.fill((0,255,255))
+	sprites = pygame.sprite.Group()
 
-	pygame.display.flip()
+	while True:
 
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+		pygame.display.flip()
+
+main()
