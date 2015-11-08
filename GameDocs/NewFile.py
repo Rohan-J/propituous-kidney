6afcf4604b6d828cd0fe984a99beb2b7afb0c605
Rohan Jayaprakash <jayaprakash.rohan@gmail.com>
2015-11-08 03:09:37 -0800

    3:09

--- GameDocs/NewFile.py
+++ GameDocs/NewFile.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, time, os, random
 
 
 
@@ -8,63 +8,147 @@ HEIGHT = 768
 DISPLAY = (WIDTH,HEIGHT)
 DEPTH = 32
 FLAGS = 0
-BLACK = (0,0,0)
-counter = 0 
+BLACK = (0, 0, 0)
+
+#global var
+jumpFlag = False
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.xc = x
+		self.yc = y
+		self.cony = y
+		self.fall = True
+		self.changeY1 = 0
+		self.changeY2 = 0
+		self.jumps = False
+		self.count = 0
+		self.index = 0
+		self.playerSprites = []
+		self.image1 = pygame.image.load("move1.png")
+		self.image1 = pygame.transform.scale(self.image1, (100,100))
+		self.image2 = pygame.image.load("move2.png")
+		self.image2 = pygame.transform.scale(self.image2, (100,100))
+		self.image3 = pygame.image.load("move3.png")
+		self.image3 = pygame.transform.scale(self.image3, (100,100))
+		self.image4 = pygame.image.load("move4.png")
+		self.image4 = pygame.transform.scale(self.image4, (100,100))
+		self.rect = self.image1.get_rect()
+		self.playerSprites.append(self.image1)
+		self.playerSprites.append(self.image2)
+		self.playerSprites.append(self.image3)
+		self.playerSprites.append(self.image4)
+		self.curImage = self.playerSprites[self.index]
 
 
-class Rectangle(pygame.sprite.Sprite):
-	def __init__(self, w, h, screens, counter):
-		self.random_x = random.randint(WIDTH/2, 1366)
-		self.random_y = random.randint(HEIGHT/3, 768/2)
-		self.newRect = pygame.draw.rect(screens, BLACK, (self.random_x, self.random_y, 100, 150))
+	def jump(self):
+		self.jumps = True
 
 	def update(self):
-		self.newRect.move(self.random_x - 1, self.random_y)
-		print self.random_x - 1
-		print self.random_y
-		self.random_x = self.random_x - 1
+		global jumpFlag
+		if(self.jumps == True):
+			self.curImage = self.playerSprites[1]
+			self.yc = self.yc - self.changeY1 #asd
+			self.changeY1 = self.changeY1 - 0.35
+			if(self.changeY1 <= 0):
+				self.changeY1 = 0
+				self.jumps = False
+		if(self.jumps == False):
+			self.fall = False
+			self.curImage = self.playerSprites[1]
+			if(self.yc < HEIGHT/3*2-88):
+				self.yc = self.yc + self.changeY2
+				self.changeY2 += 0.35
+			else:
+				self.fall = True
+				jumpFlag = False
+				self.yc = HEIGHT/3*2-88
+				self.changeY2 = 0
+
+		if(self.count == 5):
+			self.index = self.index + 1
+			if(self.index == 4):
+				self.index = 0
+			self.count = 0
+		self.count = self.count + 1
+		if(self.jumps != True and self.fall == True):
+			self.curImage = self.playerSprites[self.index]
+
+class Rectangle(pygame.sprite.Sprite):
+	def __init__(self, w, h, screens):
+		pygame.sprite.Sprite.__init__(self)
+		self.counter = 0
+		self.random_x = random.randint(100, 250)
+		self.random_y = random.randint(100, 400)
+		if(self.random_x > 200):
+			self.random_y = 270
+		self.image = pygame.Surface([self.random_x, self.random_y])
+		self.rect = self.image.get_rect()
+		self.y = self.random_y
+		self.image.fill(BLACK)
+		self.selfscreen = screens
+		self.xv = WIDTH
 
 
-		
-		
+	def update(self):
+		if(self.xv > 0-self.random_x):
+			self.selfscreen.blit(self.image,(self.xv, HEIGHT/3*1))
+			#self.newRect = pygame.draw.rect(self.selfscreen, BLACK, (self.xv, HEIGHT/3*1, self.random_x, self.random_y))
+			self.xv = self.xv - 10
+		else:
+			self.kill()
+
 
 def main():
+	global jumpFlag
 	pygame.init()
-	counter = 0
-	#-Variable4
 	clock = pygame.time.Clock()
+	rate = 0
+	#-Variables
 	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
-	pygame.display.set_caption("rohan is hot")
+	pygame.display.set_caption("rohan is hot, but rayyaan is hotter")
 	screen.fill((0,255,255))
+
+	new_sprite = Player(0, HEIGHT/3*2-88)
 	sprites = pygame.sprite.Group()
-	a = pygame.draw.rect(screen, (0,0,0), (0,HEIGHT/3*2,1366,384))
-	
-
-	while counter<=2:
-		b = Rectangle(100, 100, screen, counter) 
-		counter = counter +2
-		pygame.display.update()
-		b.update()
-		
-		print ('hello')
-	
-
-		for event in pygame.event.get():
-			if event.type==pygame.QUIT:
-				pygame.quit()
-				sys.exit()
-
-		pygame.display.flip()
-		clock.tick()
+	sprites.add(Rectangle(100, 100, screen))
+	sprites.add(new_sprite)
+	blocks = pygame.sprite.Group()
+	background_image = pygame.image.load("tilepatterns_DSC9320b.jpg").convert()
 
 	while True:
+
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				pygame.quit()
 				sys.exit()
 
-main()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					if(jumpFlag == False):
+						jumpFlag = True
+						new_sprite.changeY1 = 15
+						new_sprite.changeY2 = 6
+						new_sprite.jump()
+		if(rate == 200):
+			a = Rectangle(100, 100, screen)
+			sprites.add(a)
+			blocks.add(a)
+			rate = 0
 
+		#new_sprite.checkCollision(new_sprite, blocks)
+		a = pygame.draw.rect(screen, (0,0,0), (0,HEIGHT/3*2,1366,384))
+		sprites.update()
+		screen.blit(new_sprite.curImage, (new_sprite.xc, new_sprite.yc))
+			#sys.exit()
+		rate = rate + 1
+		pygame.display.flip()
+		clock.tick(60)
+		screen.blit(background_image,[0,0])
+		print("a")
+
+main()
 
 
 
--- GameDocs/testingstuff.py
+++ GameDocs/testingstuff.py
@@ -1,36 +1,106 @@
+"""
+  Sample code shows "Game Over" message.
+ 
+  Sample Python/Pygame Programs
+  Simpson College Computer Science
+  http://programarcadegames.com/
+  http://simpson.edu/computer-science/
+"""
+ 
 import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+ 
 pygame.init()
-
-class button_example():
-    def __init__(self):
-        self.main()
-
-    def display(self): #creating a display
-        self.screen = pygame.display.set.mode((500,500), 0, 32)
-        pygame.display.set_caption("this is the title page")
-
-    def update_display(self):
-        self.screen.fill(0,255,255)
-        self.Button1.create_button(self.screen, (0, 0, 0), 150, 200, 200, 100, 0, "New Game", (255,255,255))
-        self.Button2.create_button(self.screen, (250,250,250), 125, 50, 150, 75, 0, "Continue Game", (0,0,0))
-        pygame.display.flip()
-
-    def main(self):
-        self.Button1 = Buttons.Button()
-        self.Button2 = Buttons.Button()
-        self.display
-        while True:
-            self.update_display()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.quit()
-                elif event.type == MOUSEBUTTONDOWN:
-                    if self.Button1.pressed(pygame.mouse.get_pos()):
-                        print "New Game Has Begun!"
-                    if self.Button2.pressed(pygame.mouse.get_pos()):
-                        print "Now Contunuing Game!"
-
-button = button_example()
-
-
+ 
+# Set the height and width of the screen
+size = [700, 500]
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Game Over Example")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# Starting position of the rectangle
+rect_x = 50
+rect_y = 50
+ 
+# Speed and direction of rectangle
+rect_change_x = 5
+rect_change_y = 5
+ 
+# This is a font we use to draw text on the screen (size 36)
+font = pygame.font.Font(None, 36)
+ 
+# Use this boolean variable to trigger if the game is over.
+game_over = False
+ 
+# -------- Main Program Loop -----------
+while not done:
+ 
+    # --- Event Processing
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+        # We will use a mouse-click to signify when the game is
+        # over. Replace this, and set game_over to true in your
+        # own game when you know the game is over. (Like lives==0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            game_over = True
+ 
+    # --- Game Logic
+ 
+    # Only move and process game logic if the game isn't over.
+    if not game_over:
+        # Move the rectangle starting point
+        rect_x += rect_change_x
+        rect_y += rect_change_y
+ 
+        # Bounce the ball if needed
+        if rect_y > 450 or rect_y < 0:
+            rect_change_y = rect_change_y * -1
+        if rect_x > 650 or rect_x < 0:
+            rect_change_x = rect_change_x * -1
+ 
+    # --- Draw the frame
+ 
+    # Set the screen background
+    screen.fill(BLACK)
+ 
+    # Draw the rectangle
+    pygame.draw.rect(screen, GREEN, [rect_x, rect_y, 50, 50])
+ 
+    if game_over:
+        # If game over is true, draw game over
+        text = font.render("Game Over", True, WHITE)
+        text_rect = text.get_rect()
+        text_x = screen.get_width() / 2 - text_rect.width / 2
+        text_y = screen.get_height() / 2 - text_rect.height / 2
+        screen.blit(text, [text_x, text_y])
+ 
+    else:
+        # If game isn't over, draw this stuff.
+        text = font.render("Click to end game", True, WHITE)
+        text_rect = text.get_rect()
+        text_x = screen.get_width() / 2 - text_rect.width / 2
+        text_y = screen.get_height() / 2 - text_rect.height / 2
+        screen.blit(text, [text_x, text_y])
+ 
+    # Limit frames per second
+    clock.tick(60)
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
