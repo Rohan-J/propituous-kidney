e72a6c0e26e31ae89a89e77ff0cc52d7a86f57d2
thing1st <hanmathew@yahoo.com>
2015-11-08 10:13:20 -0800

    doh

--- GameDocs/combined_main.py
+++ GameDocs/combined_main.py
@@ -1,6 +1,8 @@
 import pygame, sys, time, os, random
+pygame.init()
 
-
+pygame.mixer.music.load("NitroFun_NewGame.ogg")
+pygame.mixer.music.play(-1)
 
 #-Constants
 WIDTH = 1366
@@ -9,9 +11,27 @@ DISPLAY = (WIDTH,HEIGHT)
 DEPTH = 32
 FLAGS = 0
 BLACK = (0, 0, 0)
+Screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
 
-#global var
+#global var + rohand's stufff
 jumpFlag = False
+score=0
+black=(100,149,237)
+end_it=False
+background_image = pygame.image.load("tilepatterns_DSC9320b.jpg").convert()
+while (end_it==False):
+    Screen.blit(background_image,[0,0])
+    font = pygame.font.SysFont("Ariel", 42)
+    nlabel=font.render("Welcome! Click to Play!", 1, (0, 0, 0))
+    for event in pygame.event.get():
+    	if event.type == pygame.QUIT:
+    		pygame.quit()
+    		sys.exit()
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            end_it=True
+    Screen.blit(nlabel,[500,384])
+    pygame.display.flip()
+
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self, x, y):
@@ -57,13 +77,13 @@ class Player(pygame.sprite.Sprite):
 		if(self.jumps == False):
 			self.fall = False
 			self.curImage = self.playerSprites[1]
-			if(self.yc < HEIGHT/3*2-88):
+			if(self.yc < HEIGHT/3*2-100):
 				self.yc = self.yc + self.changeY2
 				self.changeY2 += 0.35
 			else:
 				self.fall = True
 				jumpFlag = False
-				self.yc = HEIGHT/3*2-88
+				self.yc = HEIGHT/3*2-100
 				self.changeY2 = 0
 
 		if(self.count == 5):
@@ -79,10 +99,16 @@ class Rectangle(pygame.sprite.Sprite):
 	def __init__(self, w, h, screens):
 		pygame.sprite.Sprite.__init__(self)
 		self.counter = 0
-		self.random_x = random.randint(100, 250)
-		self.random_y = random.randint(100, 400)
+		self.random_x = random.randint(100, 150)
+		self.random_y = random.randint(100, 350)
+		self.inOut = random.randint(1,2)
+		self.fadeCount = 0
+		if(self.inOut == 1):
+			self.opacity = 255
+		elif(self.inOut == 2):
+			self.opacity = 1
 		if(self.random_x > 200):
-			self.random_y = 270
+			self.random_y = 140
 		self.image = pygame.Surface([self.random_x, self.random_y])
 		self.rect = self.image.get_rect()
 		self.y = self.random_y
@@ -93,30 +119,76 @@ class Rectangle(pygame.sprite.Sprite):
 
 	def update(self):
 		if(self.xv > 0-self.random_x):
-			self.selfscreen.blit(self.image,(self.xv, HEIGHT/3*1))
-			#self.newRect = pygame.draw.rect(self.selfscreen, BLACK, (self.xv, HEIGHT/3*1, self.random_x, self.random_y))
+			self.image.set_alpha(self.opacity)
+			self.selfscreen.blit(self.image,(self.xv, HEIGHT/3))
 			self.xv = self.xv - 10
+			if(self.inOut == 1):
+				self.opacity = self.opacity - 3
+			elif(self.inOut == 2):
+				if(self.fadeCount >= 60):
+					self.opacity = self.opacity + 2
+				self.fadeCount += 1
 		else:
 			self.kill()
 
 
+def gameOver():
+	global score
+	font = pygame.font.Font(None, 36)
+	clicked = False
+	endScore = font.render("Final Score: " + str(score), True, BLACK)
+	endScore_rect = endScore.get_rect()
+	endScore_x= WIDTH/2 - endScore_rect.width/2
+	text = font.render("Game Over! Click to exit.", True, BLACK)
+	text_rect = text.get_rect()
+	text_x = WIDTH/2 - text_rect.width/2
+	text_y = HEIGHT/2 - text_rect.height/2
+	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
+	background_image = pygame.image.load("tilepatterns_DSC9320b.jpg").convert()
+	screen.fill((0,255,255))
+	while(not clicked):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				clicked = True
+		screen.blit(text, [text_x, text_y])
+		screen.blit(endScore, [text_x, 300])
+		pygame.display.flip()
+		screen.blit(background_image,[0,0])
+
+
 def main():
+
+	lives = 3
+	done = False
+	global score
 	global jumpFlag
 	pygame.init()
+	collided = False
 	clock = pygame.time.Clock()
+	font = pygame.font.Font(None,36)
 	rate = 0
-	#-Variables
+	definedRate = 120
+	limit = 0
+	upgrade = 0
+
 	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
-	pygame.display.set_caption("rohan is hot, but rayyaan is hotter")
+	pygame.display.set_caption("FADE")
 	screen.fill((0,255,255))
 
-	new_sprite = Player(0, HEIGHT/3*2-88)
+	new_sprite = Player(0, HEIGHT/3*2-100)
 	sprites = pygame.sprite.Group()
-	sprites.add(Rectangle(100, 100, screen))
-	sprites.add(new_sprite)
 	blocks = pygame.sprite.Group()
+	a = Rectangle(100, 100, screen)
+	sprites.add(a)
+	sprites.add(new_sprite)
+	blocks.add(a)
+	background_image = pygame.image.load("tilepatterns_DSC9320b.jpg").convert()
 
-	while True:
+
+	while not done:
 
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
@@ -130,21 +202,48 @@ def main():
 						new_sprite.changeY1 = 15
 						new_sprite.changeY2 = 6
 						new_sprite.jump()
-		if(rate == 200):
+
+		for x in blocks:
+			if(new_sprite.yc > x.y and x.xv <= 100 and limit > 60):
+				if(HEIGHT/3-x.y > 100):
+					pass
+				else:
+					print("You crashed")
+					limit = 0
+					lives = lives - 1
+					if(lives == 0):
+						done = True
+
+
+
+		if(rate == definedRate): #150
+			if(upgrade == 10):
+				definedRate = definedRate - 15
+				upgrade = 0
 			a = Rectangle(100, 100, screen)
 			sprites.add(a)
 			blocks.add(a)
+			#print(HEIGHT/3*1-a.y-100)
 			rate = 0
+			upgrade = upgrade + 1
 
+
+		livesText = font.render("Lives: " + str(lives), True, BLACK)
+		lives_rect = livesText.get_rect()
+		scoreText = font.render("Score: " + str(score), True, BLACK)
+		score_rect = scoreText.get_rect()
 		#new_sprite.checkCollision(new_sprite, blocks)
 		a = pygame.draw.rect(screen, (0,0,0), (0,HEIGHT/3*2,1366,384))
 		sprites.update()
 		screen.blit(new_sprite.curImage, (new_sprite.xc, new_sprite.yc))
+		screen.blit(livesText, [WIDTH-200, 50])
+		screen.blit(scoreText, [10, 50])
 			#sys.exit()
 		rate = rate + 1
+		limit = limit + 1
+		score = score + 1
 		pygame.display.flip()
 		clock.tick(60)
-		screen.fill((0,255,255))
-		print("a")
-
+		screen.blit(background_image,[0,0])
 main()
+gameOver()
--- GameDocs/main_mathew.py
+++ GameDocs/main_mathew.py
@@ -1,6 +1,8 @@
 import pygame, sys, time, os, random
+pygame.init()
 
-
+pygame.mixer.music.load("NitroFun_NewGame.ogg")
+pygame.mixer.music.play(-1)
 
 #-Constants
 WIDTH = 1366
@@ -9,9 +11,27 @@ DISPLAY = (WIDTH,HEIGHT)
 DEPTH = 32
 FLAGS = 0
 BLACK = (0, 0, 0)
+Screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
 
-#global var
+#global var + rohand's stufff
 jumpFlag = False
+score=0
+black=(100,149,237)
+end_it=False
+background_image = pygame.image.load("tilepatterns_DSC9320b.jpg").convert()
+while (end_it==False):
+    Screen.blit(background_image,[0,0])
+    font = pygame.font.SysFont("Ariel", 42)
+    nlabel=font.render("Welcome! Click to Play!", 1, (0, 0, 0))
+    for event in pygame.event.get():
+    	if event.type == pygame.QUIT:
+    		pygame.quit()
+    		sys.exit()
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            end_it=True
+    Screen.blit(nlabel,[500,384])
+    pygame.display.flip()
+
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self, x, y):
@@ -81,6 +101,12 @@ class Rectangle(pygame.sprite.Sprite):
 		self.counter = 0
 		self.random_x = random.randint(100, 150)
 		self.random_y = random.randint(100, 350)
+		self.inOut = random.randint(1,2)
+		self.fadeCount = 0
+		if(self.inOut == 1):
+			self.opacity = 255
+		elif(self.inOut == 2):
+			self.opacity = 1
 		if(self.random_x > 200):
 			self.random_y = 140
 		self.image = pygame.Surface([self.random_x, self.random_y])
@@ -93,16 +119,26 @@ class Rectangle(pygame.sprite.Sprite):
 
 	def update(self):
 		if(self.xv > 0-self.random_x):
+			self.image.set_alpha(self.opacity)
 			self.selfscreen.blit(self.image,(self.xv, HEIGHT/3))
-			#self.newRect = pygame.draw.rect(self.selfscreen, BLACK, (self.xv, HEIGHT/3*1, self.random_x, self.random_y))
 			self.xv = self.xv - 10
+			if(self.inOut == 1):
+				self.opacity = self.opacity - 3
+			elif(self.inOut == 2):
+				if(self.fadeCount >= 60):
+					self.opacity = self.opacity + 2
+				self.fadeCount += 1
 		else:
 			self.kill()
 
 
 def gameOver():
+	global score
 	font = pygame.font.Font(None, 36)
 	clicked = False
+	endScore = font.render("Final Score: " + str(score), True, BLACK)
+	endScore_rect = endScore.get_rect()
+	endScore_x= WIDTH/2 - endScore_rect.width/2
 	text = font.render("Game Over! Click to exit.", True, BLACK)
 	text_rect = text.get_rect()
 	text_x = WIDTH/2 - text_rect.width/2
@@ -118,6 +154,7 @@ def gameOver():
 			elif event.type == pygame.MOUSEBUTTONDOWN:
 				clicked = True
 		screen.blit(text, [text_x, text_y])
+		screen.blit(endScore, [text_x, 300])
 		pygame.display.flip()
 		screen.blit(background_image,[0,0])
 
@@ -126,15 +163,19 @@ def main():
 
 	lives = 3
 	done = False
+	global score
 	global jumpFlag
 	pygame.init()
 	collided = False
 	clock = pygame.time.Clock()
+	font = pygame.font.Font(None,36)
 	rate = 0
+	definedRate = 120
 	limit = 0
+	upgrade = 0
 
 	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
-	pygame.display.set_caption("Run")
+	pygame.display.set_caption("FADE")
 	screen.fill((0,255,255))
 
 	new_sprite = Player(0, HEIGHT/3*2-100)
@@ -163,10 +204,10 @@ def main():
 						new_sprite.jump()
 
 		for x in blocks:
-			if(new_sprite.yc > x.y and x.xv <= 100 and x.xv >= 0):
+			if(new_sprite.yc > x.y and x.xv <= 100 and limit > 60):
 				if(HEIGHT/3-x.y > 100):
 					pass
-				elif(limit > 60):
+				else:
 					print("You crashed")
 					limit = 0
 					lives = lives - 1
@@ -175,21 +216,32 @@ def main():
 
 
 
-		if(rate == 120): #150
+		if(rate == definedRate): #150
+			if(upgrade == 10):
+				definedRate = definedRate - 15
+				upgrade = 0
 			a = Rectangle(100, 100, screen)
 			sprites.add(a)
 			blocks.add(a)
 			#print(HEIGHT/3*1-a.y-100)
 			rate = 0
+			upgrade = upgrade + 1
 
 
+		livesText = font.render("Lives: " + str(lives), True, BLACK)
+		lives_rect = livesText.get_rect()
+		scoreText = font.render("Score: " + str(score), True, BLACK)
+		score_rect = scoreText.get_rect()
 		#new_sprite.checkCollision(new_sprite, blocks)
 		a = pygame.draw.rect(screen, (0,0,0), (0,HEIGHT/3*2,1366,384))
 		sprites.update()
 		screen.blit(new_sprite.curImage, (new_sprite.xc, new_sprite.yc))
+		screen.blit(livesText, [WIDTH-200, 50])
+		screen.blit(scoreText, [10, 50])
 			#sys.exit()
 		rate = rate + 1
 		limit = limit + 1
+		score = score + 1
 		pygame.display.flip()
 		clock.tick(60)
 		screen.blit(background_image,[0,0])
